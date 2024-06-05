59a37d305e566e5b5853019253672745e9e90920
Brobo1 <armanahz95@gmail.com>
2024-06-05 16:56:19 +0200

    Update removeFromArray function and test cases
    The removeFromArray function has been updated to remove specified elements from the given array. Additionally, the test cases that were previously skipped in removeFromArray.spec.js have been enabled for verification of the updates to the function.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...rem) {
+  for (const remElement of rem) {
+    arr = arr.filter((num) => num !== remElement);
+  }
+
+  // rem.map((num) => {
+  //   arr = arr.filter((filt) => filt !== num);
+  // });
+  return arr;
 };
 
 // Do not edit below this line
