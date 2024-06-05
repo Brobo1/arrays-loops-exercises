b8863427bd2969f6b5338b53c99036993513ebcc
Brobo1 <armanahz95@gmail.com>
2024-06-05 18:21:42 +0200

    Update palindrome function to handle cases
    Updated the palindrome function to correctly handle test cases with punctuation, upper-case letters, and multiple words. Also, modified the test suite to enable previously skipped test cases. The palindrome function now also correctly deals with numbers within strings.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.replace(/[^A-Za-z\d]/g, "").toLowerCase();
+  let rev = str.split("").reverse().join("");
+  console.log(str, rev);
+  return str === rev;
 };
 
+palindromes("racecar!");
 // Do not edit below this line
 module.exports = palindromes;
