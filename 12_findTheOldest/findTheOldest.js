e31bf2296190b1773d62c5ecf66b4a2c7d817318
Brobo1 <armanahz95@gmail.com>
2024-06-05 20:05:46 +0200

    Update 'findTheOldest' and 'getTheTitles' functions
    Updated the 'findTheOldest' function to properly calculate person's age and return the oldest person from an array. Also, the 'getTheTitles' function now returns a list of book titles. Minor changes are made in the associated tests for proper running and testing.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let age = [
+    ...arr.map((peeps) => {
+      return Math.abs(
+        peeps.yearOfDeath ?? new Date().getFullYear() - peeps.yearOfBirth,
+      );
+    }),
+  ];
+  console.log(age);
+  let maxAge = Math.max(...age);
+  return arr[age.indexOf(maxAge)];
 };
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+// findTheOldest(people);
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
