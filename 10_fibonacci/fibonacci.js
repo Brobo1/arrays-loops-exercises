7acd06a168c4f8621943e78d9d0d819504851f27
Brobo1 <armanahz95@gmail.com>
2024-06-05 18:21:31 +0200

    Implement Fibonacci function and update tests
    The Fibonacci function in fibonacci.js has been implemented to correctly calculate the Fibonacci sequence up to the designated number. Negative numbers are rejected with an "OOPS" message. Tests for fibonacci function in fibonacci.spec.js have been updated and all skip directives were removed.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let fib = [0, 1];
+  for (let i = 1; i <= num; i++) {
+    fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+  }
+  return num < 0 ? "OOPS" : fib[num];
 };
 
 // Do not edit below this line
